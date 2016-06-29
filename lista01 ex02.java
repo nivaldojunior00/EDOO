b0ede1f4f2a231655d252139a2b0234cf60bfb89
Nivaldo Júnior <nvaldojunior00@gmail.com>
2016-06-29 01:52:55 -0300

    Fila em Java
    Estudando filas em Java

--- lista01 ex02.java
+++ lista01 ex02.java
@@ -1,9 +1,18 @@
+public class objeto{
+	public int id;
+	public string mensagem;
+	public date dataEnvio;
+		
+	}
+
 public class Fila {
 
   private List<Object> objetos = new LinkedList<Object>();
 
-  public void insere(Object objeto) {
-    this.objetos.add(objeto);
+  public void insere(int id, string mensagem, date dataEnvio) {
+    this.objetos.id.add(id);
+    this.objetos.mensagem.add(mensagem);
+    this.objetos.dataEnvio.add(dataEnvio);
   }
 
   public Object remove() {
